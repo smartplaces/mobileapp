f3f06be53aebba8425165061fefa92facda461ae
yamironov <yamironov@gmail.com>
2014-11-26 15:18:01 +0300

    Initial commit (copy from website repository)

---
+++ js/my-app.js
@@ -0,0 +1,15 @@
+// Initialize your app
+var myApp = new Framework7({
+  swipeBackPage: false
+});
+
+// Export selectors engine
+var $$ = Framework7.$;
+
+// Add views
+var view1 = myApp.addView('#view-1');
+var view2 = myApp.addView('#view-2');
+var view3 = myApp.addView('#view-3');
+var view4 = myApp.addView('#view-4');
+
+var historySlider = myApp.slider('.places-history', {pagination: '.slider-pagination', onSlideChangeEnd: function(slider) {myApp.showTab('#place' + slider.activeSlideIndex);}})
---
+++ js/ppt.js
@@ -0,0 +1,10 @@
+// Initialize your app
+var myApp = new Framework7({
+  swipeBackPage: false
+});
+
+// Export selectors engine
+var $$ = Framework7.$;
+
+// Add views
+var view1 = myApp.addView('#view-1');
